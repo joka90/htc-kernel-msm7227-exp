9b0ee8dc4f60981dc534de38f5a4e1410e6e8421
Johan Källström <joka90@gmail.com>
2011-07-28 01:15:44 +0200

    Hacked up msm_wifi from s0be; Add htc_wifi_nvs.o to CONFIG_TROUT_WIFI to get proper load

--- arch/arm/mach-msm/msm_wifi.c
+++ arch/arm/mach-msm/msm_wifi.c
@@ -19,6 +19,51 @@
  */
 #include <linux/platform_device.h>
 #include <linux/spi/wl12xx.h>
+#include <linux/proc_fs.h>
+
+extern unsigned char *get_wifi_nvs_ram(void);
+static struct proc_dir_entry *msm_wifi_calibration;
+
+#define WIFI_NVS_LEN_OFFSET     0x0C
+#define WIFI_NVS_DATA_OFFSET    0x40
+#define WIFI_NVS_MAX_SIZE       0x800UL
+
+static unsigned long tiwlan_get_nvs_size( void )
+{
+    unsigned char *ptr;
+    unsigned long len;
+
+    ptr = get_wifi_nvs_ram();
+    if( ptr == NULL ) {
+        return 0;
+    }
+    /* Size in format LE assumed */
+    memcpy( (void *)&len, (void *)(ptr + WIFI_NVS_LEN_OFFSET), sizeof(len) );
+    len = min( len, (WIFI_NVS_MAX_SIZE-WIFI_NVS_DATA_OFFSET) );
+    return len;
+}
+
+static int tiwlan_calibration_read_proc(char *page, char **start, off_t off,
+                                int count, int *eof, void *data)
+{
+    unsigned char *ptr;
+    unsigned long len;
+
+    ptr = get_wifi_nvs_ram();
+    if( ptr == NULL ) {
+        return 0;
+    }
+    len = tiwlan_get_nvs_size();
+    /* i += sprintf(page+i, "WiFi Calibration Size = %lu %x bytes\n", len); */
+    memcpy( (void *)page, (void *)(ptr + WIFI_NVS_DATA_OFFSET), len );
+    return len;
+}
+
+static int tiwlan_calibration_write_proc(struct file *file, const char *buffer,
+                                 unsigned long count, void *data)
+{
+    return 0;
+}
 
 static int wifi_probe(struct platform_device *pdev)
 {
@@ -69,12 +114,20 @@ static struct platform_driver wifi_device = {
 
 static int __init msm_wifi_sdio_init(void)
 {
+        msm_wifi_calibration = create_proc_entry("msm_wifi_calibration", 0644, NULL);
+	if (msm_wifi_calibration) {
+		msm_wifi_calibration->size = tiwlan_get_nvs_size();
+		msm_wifi_calibration->read_proc = tiwlan_calibration_read_proc;
+		msm_wifi_calibration->write_proc = tiwlan_calibration_write_proc;
+	}
 	return platform_driver_register(&wifi_device);
 }
 
 static void __exit msm_wifi_sdio_exit(void)
 {
 	platform_driver_unregister(&wifi_device);
+	if(msm_wifi_calibration)      /* After renaming */
+		remove_proc_entry("msm_wifi_calibration", NULL);
 }
 
 module_init(msm_wifi_sdio_init);
